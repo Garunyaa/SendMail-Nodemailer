2b712a2187c3429f4996fcc586594bc1ce6621f0
Garunyaa <garunyaas@gmail.com>
2023-09-25 14:16:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const emails = ["smokemax2002@gmail.com", "garushanmugam@gmail.com"];
+
+let mailSender = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.user,
+    pass: process.env.pass,
+  },
+  tls: { rejectUnauthorized: false },
+});
+
+let details = {
+  from: "teddygks@gmail.com",
+  to: emails,
+  subject: "Hey Minion!",
+  attachments: [
+    {
+      filename: "minion.jpg",
+      path: "minion.jpg",
+      cid: "image",
+    },
+  ],
+  html: `<h4>Thanks for reading this email.</h4>
+        <p><img src = 'cid:image'></img></p> `,
+};
+
+mailSender.sendMail(details, (error) => {
+  if (error) {
+    console.log("Error sending mail");
+  } else {
+    console.log("Email sent successfully");
+  }
+});
